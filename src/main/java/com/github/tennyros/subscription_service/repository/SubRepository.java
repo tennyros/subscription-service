53fa27c2fe6c366dd0a90e5025dedd11230a1266
tennyros <vvkamalov_obs@mail.ru>
2025-05-07 01:00:28 +0500

    repository: add query for top 3 subs, find by user id method declaration

--- src/main/java/com/github/tennyros/subscription_service/repository/SubRepository.java
+++ src/main/java/com/github/tennyros/subscription_service/repository/SubRepository.java
@@ -2,8 +2,16 @@ package com.github.tennyros.subscription_service.repository;
 
 import com.github.tennyros.subscription_service.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SubRepository extends JpaRepository<Subscription, Long> {
+
+    @Query("SELECT s.serviceName, COUNT(s) as count FROM Subscription s GROUP BY s.serviceName ORDER BY count DESC LIMIT 3")
+    List<Object[]> findTop3PopularSubscriptions();
+
+    List<Subscription> findByUserId(Long userId);
 }
