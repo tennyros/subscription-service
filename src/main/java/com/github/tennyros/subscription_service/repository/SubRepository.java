d095870b788d7150cb9fc26ea431136f6f9171a4
tennyros <vvkamalov_obs@mail.ru>
2025-05-07 10:46:03 +0500

    repositories: add delete suscription method by user id and sub id, change get top subscriptions query according to projection

--- src/main/java/com/github/tennyros/subscription_service/repository/SubRepository.java
+++ src/main/java/com/github/tennyros/subscription_service/repository/SubRepository.java
@@ -1,8 +1,11 @@
 package com.github.tennyros.subscription_service.repository;
 
+import com.github.tennyros.subscription_service.repository.projection.TopSubscriptionsProjection;
 import com.github.tennyros.subscription_service.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +13,16 @@ import java.util.List;
 @Repository
 public interface SubRepository extends JpaRepository<Subscription, Long> {
 
-    @Query("SELECT s.serviceName, COUNT(s) as count FROM Subscription s GROUP BY s.serviceName ORDER BY count DESC LIMIT 3")
-    List<Object[]> findTop3PopularSubscriptions();
+    @Query("""
+            SELECT s.serviceName as serviceName, COUNT(s) as count
+            FROM Subscription s GROUP BY s.serviceName
+            ORDER BY count DESC LIMIT 3
+            """)
+    List<TopSubscriptionsProjection> findTop3PopularSubscriptions();
+
+    @Modifying
+    @Query("DELETE FROM Subscription s WHERE s.id = :subId AND s.user.id = :userId")
+    int deleteByIdAndUserId(@Param("subId") Long subId, @Param("userId") Long userId);
 
     List<Subscription> findByUserId(Long userId);
 }
