fe35227a7c321ea4a9dec878bf4225453081961d
tennyros <vvkamalov_obs@mail.ru>
2025-05-07 14:10:05 +0500

    model: add lazy fetch in subscription list of user entity

--- src/main/java/com/github/tennyros/subscription_service/model/User.java
+++ src/main/java/com/github/tennyros/subscription_service/model/User.java
@@ -2,6 +2,7 @@ package com.github.tennyros.subscription_service.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -36,7 +37,7 @@ public class User {
     private String email;
 
     @Builder.Default
-    @OneToMany(mappedBy = "user", cascade = { ALL }, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = { ALL }, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Subscription> subscriptions = new ArrayList<>();
 
 }
